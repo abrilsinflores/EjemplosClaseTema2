04d3899c43ef3d45e9bcfc2f3d45e8a63bb94d68
abrilsinflores <abril.perez@alumnos.upm.es>
2025-09-28 14:27:04 +0200

    subida archivos EjemploClaseTema2
    copia del código que se hizo en clase

--- es/upm/aled/recursion/backtrack/Coordinate.java
+++ es/upm/aled/recursion/backtrack/Coordinate.java
@@ -1,43 +1,42 @@
 package es.upm.aled.recursion.backtrack;
 
+import java.util.Objects;
+
 public class Coordinate {
+
+	private int x;
+	private int y;
 	
 	public Coordinate(int x, int y) {
 		super();
 		this.x = x;
 		this.y = y;
 	}
-	private int x;
-	private int y;
+
 	public int getX() {
 		return x;
 	}
-	public void setX(int x) {
-		this.x = x;
-	}
+
 	public int getY() {
 		return y;
 	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	
-	
-	
-	public boolean equals(Object obj) {
-		if (this==obj)
-			return true;
-		if(obj==null)
-				return false;
-		if(this.getClass() )
-		
-		Coordinate other = (Coordinate) obj; // hacemos casting xq Java no sabe de qué clase es el objeto
-		return((this.x == other.x) && (this.y == other.y));
-	}
+
 	@Override
 	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
+		return Objects.hash(x, y);
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Coordinate other = (Coordinate) obj;
+		return ( (this.x == other.x) && (this.y == other.y) );
+	}
+	
+	
 }
--- es/upm/aled/recursion/backtrack/Test.java
+++ es/upm/aled/recursion/backtrack/Test.java
@@ -3,16 +3,9 @@ package es.upm.aled.recursion.backtrack;
 public class Test {
 
 	public static void main(String[] args) {
-		Coordinate sitio1 = new Coordinate(4,3);
-		Coordinate sitio2 = new Coordinate(4,3);
-		boolean iguales = (sitio1==sitio2);
-		System.out.println("son iguales?: "+iguales); // sale false, tendrán distinta direcc d memoria usamos el equals
-		boolean igualesConEquals = (sitio1.equals(sitio2));
-		System.out.println("son iguales?: "+igualesConEquals); // sale false tmb xq no hemos q definido  el equals
-
-
-		
-		
+		Coordinate sitio1 = new Coordinate(4, 3);
+		Coordinate sitio2 = new Coordinate(4, 3);
+		boolean iguales = (sitio1.equals(sitio2));
+		System.out.println("¿Son iguales?: " + iguales);
+		}
 	}
-
-}
