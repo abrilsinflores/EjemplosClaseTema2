6fb466c4a6c3ef5e3b31d5f9089607f5522427ed
abrilsinflores <abril.perez@alumnos.upm.es>
2025-09-28 14:31:39 +0200

    adición de la clase Persona a EjemploClaseTema2

---
+++ es/upm/es/aled/recursion/data/Persona.java
@@ -0,0 +1,49 @@
+package es.upm.es.aled.recursion.data;
+
+import java.util.List;
+
+public class Persona {
+
+	private String nombre;
+	private List<Persona> hijos;
+	
+	public Persona(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public List<Persona> getHijos() {
+		return hijos;
+	}
+
+	public void addHijo(Persona p) {
+		// TODO 
+	}
+
+	public String descendientes() {
+		// TODO: Devuelve un String con todos los descendientes
+		// Seguro que hay que llamar de nuevo a descendientes()
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona ro = new Persona("Rodrigo");
+		Persona no = new Persona("Nora");
+		ro.addHijo(no);
+		Persona lu = new Persona("Luis");
+		no.addHijo(lu);
+		Persona ra = new Persona("Ramona");
+		no.addHijo(ra);
+		System.out.println(ro.descendientes());
+		
+		
+		
+		
+	}
+
+}
