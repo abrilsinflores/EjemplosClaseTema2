ecd1a0349f053b66bd6b1e341f47690c74f61eb4
abrilsinflores <abril.perez@alumnos.upm.es>
2025-10-16 00:19:44 +0200

    rectificación método bubblesort
    ahora sí funciona

--- es/upm/aled/complejidad/BubbleSort.java
+++ es/upm/aled/complejidad/BubbleSort.java
@@ -1,18 +1,25 @@
 package es.upm.aled.complejidad;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 	
 	//definición del método bubble sort con complejidad algorítmica O(n^2)
 	//me dan un array de ints desordenados y devuelvo uno ordenado
 	
-	public int[] bubbleSort(int[] arrayDesordenado) {
+	public static int[] bubbleSort(int[] arrayDesordenado) {
 		int[] arrayOrdenado = arrayDesordenado;
-		for(int i = 0; i<arrayOrdenado.length; i++) {
-			int valor = arrayOrdenado[i];
-			if(valor>arrayOrdenado[i+1]) {
-				arrayOrdenado[i] = arrayOrdenado[i+1];
-				arrayOrdenado[i+1] = valor;
+		//para hacer el código más legible:
+		int n = arrayOrdenado.length; 
+		for(int j = 0; j<n; j++) { //recorremos el array tantas veces como su longitud
+			for(int i = 0; (i+1)<n; i++) {
+				int valor = arrayOrdenado[i];
+				if(valor>arrayOrdenado[i+1]) {
+					arrayOrdenado[i] = arrayOrdenado[i+1];
+					arrayOrdenado[i+1] = valor;
+				}
 			}
+			
 		}
 		
 		return arrayOrdenado;
@@ -21,8 +28,8 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		
-		
-
+		int[] miArray = {4,3,45,9,777,5,2,3};
+		System.out.println(Arrays.toString(BubbleSort.bubbleSort(miArray)));
 	}
 
 }
